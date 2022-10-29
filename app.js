3c6f668521defcf1f7bd71c5b57a52db3f9bbe4d
Jonathan Clevenger <jon@clevenger.to>
2022-10-29 12:26:41 -0500

    changed alert

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 console.log('hola there');
 let btn = document.querySelector('button')
 btn.onclick = function() {
-    alert('Thanks for asking, but we cannot answer that question (yet)');
+    alert('Thanks for asking, but unfortunately we cannot answer that question (yet)');
 }
